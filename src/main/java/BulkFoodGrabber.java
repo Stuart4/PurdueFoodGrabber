6fee50d80b2936798e594eb450c46c90065a4e9c
Stuart4 <stuart4@purdue.edu>
2014-12-01 14:47:16 -0500

    adds license to BulkFoodGrabber.java

--- src/main/java/BulkFoodGrabber.java
+++ src/main/java/BulkFoodGrabber.java
@@ -3,9 +3,20 @@ import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-/**
- * Created by jake on 12/1/14.
- */
+/*This file is part of PurdueFoodGrabber.
+
+		PurdueFoodGrabber is free software: you can redistribute it and/or modify
+		it under the terms of the GNU General Public License as published by
+		the Free Software Foundation, either version 3 of the License, or
+		(at your option) any later version.
+
+		PurdueFoodGrabber is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU General Public License for more details.
+
+		You should have received a copy of the GNU General Public License
+		along with PurdueFoodGrabber.  If not, see <http://www.gnu.org/licenses/>. */
 public class BulkFoodGrabber {
 	private final SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
 	private Calendar cal;
